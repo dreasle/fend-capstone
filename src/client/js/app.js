2477b5a95928713f74462dd2eb92a2f5302d959e
Andrea Lehman <andy.l.lehman@gmail.com>
2020-01-02 19:41:36 -0800

    feat: Adding the geonames api.
    Creating geonames class that is called from the main app.js
    The api returns lat, long, state and country and the UI is updated
    with that info.

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,81 +1,21 @@
-// Load environment variables
-// const dotenv2 = require('dotenv')
-// dotenv2.config()
+import { geonames } from './geonames';
 
-// OpenWeatherMap API Key
-const apiKey = '87a3a1d8611b3598ddddf467d6e956ed';
-// const apiKey = process.env.API_KEY;
-const baseURL = 'http://api.openweathermap.org/data/2.5/weather?';
+function updateCityUI(cityInfo) {
+    document.getElementById('lat').innerHTML = `Lat: ${cityInfo.lat}`;
+    document.getElementById('long').innerHTML = `Long: ${cityInfo.long}`;
+    document.getElementById('state').innerHTML = `State: ${cityInfo.state}`;
+    document.getElementById('country').innerHTML = `Country: ${cityInfo.country}`;
 
-// Event listener for generate click event
-// console.log('about to add click event listener')
-// document.getElementById('generate').addEventListener('click', updateApp);
+}
 
 // Function to run when generate button is clicked
 function updateApp(e){
-    console.log('in updateApp: ', e)
-    // Get zip and feelings from UI
-    const zip =  document.getElementById('zip').value;
-    const feelings =  document.getElementById('feelings').value;
-
-    // Get weather from api
-    getWeather(baseURL, zip, apiKey)
-    .then(function(data){
-        // Update the data obj on the server
-        const currentDate = new Date(data.dt*1000).toLocaleDateString("en-US");
-        updateData('http://localhost:8082/save', {
-        // updateData('/save', {
-            date: currentDate, 
-            temp: data.main.temp,
-            feelings: feelings
-        });
-    })
-    .then(function(data){
-        // Update the UI
-        updateUI();
-    });
-}
-
-// Async GET request
-const getWeather = async (baseURL, zip, apiKey)=>{
-    const res = await fetch(`${baseURL}zip=${zip},us&APPID=${apiKey}&units=imperial`)
-    try {
-        const data = await res.json();
-        return data;
-    } catch(error) {
-        console.log("error", error);
-    }
-}
-
-// Async POST request
-const updateData = async ( url = '', data = {})=>{
-
-    try {
-        const response = await fetch(url, {
-            method: 'POST', 
-            credentials: 'same-origin', 
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(data)
-        });
-    } catch(error) {
-        console.log("error", error);
-    }
-};
-
-// Update the UI/HTML withe the server-side data obj.
-const updateUI = async () => {
-  const request = await fetch('http://localhost:8082/all');
-//   const request = await fetch('/all');
-  try {
-    const newData = await request.json();
-    document.getElementById('date').innerHTML = `Date: ${newData.date}`;
-    document.getElementById('temp').innerHTML = `Temp: ${newData.temp} F`;
-    document.getElementById('content').innerHTML = `Feelings: ${newData.feelings}`;
-  } catch(error) {
-    console.log("error", error);
-  }
+    // Get city info from geonames
+    const city =  document.getElementById('zip').value;
+    console.log("city: ", city)
+    const cityInfo = geonames.get(city)
+    .then(cityInfo => updateCityUI(cityInfo))
+    .catch(err => console.log(err));
 }
 
 export { updateApp }
